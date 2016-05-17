2e5f0f1633edf31fad1e3317b01a878ecb71618b
Artem Polishchuk (Kharkiv) <a.polishchuk@mobidev.biz>
2016-05-17 15:35:57 +0300

    - include jade templates (pug files)

--- app.js
+++ app.js
@@ -1,9 +1,4 @@
 var express = require('express');
-
-var app = express();
-
-
-
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
@@ -13,14 +8,15 @@ var bodyParser = require('body-parser');
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
-
+var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set('view engine', 'pug');
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public/images', 'favicon.ico')));
+console.log(path.join(__dirname, 'public/images', 'favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
