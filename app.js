4908d988df8c649e3a440ebda69e0d5c4a5753b0
stich6269 <stich6269@mail.ru>
2016-05-18 20:14:25 +0300

    - added logger for catch errors

--- app.js
+++ app.js
@@ -4,10 +4,9 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var HttpError = require('./error').HttpError;
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
-
+var routes = require('./routes');
 var app = express();
 
 // view engine setup
@@ -16,46 +15,32 @@ app.set('view engine', 'pug');
 
 // uncomment after placing your favicon in /public
 app.use(favicon(path.join(__dirname, 'public/images', 'favicon.ico')));
-console.log(path.join(__dirname, 'public/images', 'favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+//Directions of requests
 app.use('/', routes);
-app.use('/users', users);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+app.use(function (req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
-
-// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
-  });
-}
 
 // production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
-});
+app.use(function (err, req, res, next) {
+    console.log('prod errors ...');
 
+    if(err instanceof HttpError){
+        res.render('error', {error: err, title: 'error'});
+    }else{
+        res.status(err.status || 500);
+        res.json(error);
+    }
+});
 
 module.exports = app;
